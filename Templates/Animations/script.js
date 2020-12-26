fcdc9cf970398d656da83c66a86437f928e9e376
Tommy Hoang <tommyyhoangg@gmail.com>
2020-12-26 15:53:18 +0100

    First commit

---
+++ Templates/Animations/script.js
@@ -0,0 +1,34 @@
+// Nav - burger
+const burger = document.querySelector(".burger-menu");
+const burgerMenu = document.querySelector(".nav-menu");
+
+burger.addEventListener("click", () => {
+   burger.classList.toggle("burger-open");
+   burgerMenu.classList.toggle("openM");
+
+   if (burger.classList !== "burger-open") {
+      productsMenu.classList.remove("products-open");
+   }
+});
+
+// Nav - products
+const products = document.querySelector(".link-products");
+const productsMenu = document.querySelector(".nav-products")
+
+products.addEventListener("click", () => {
+   productsMenu.classList.toggle("products-open");
+});
+
+const goBack = document.querySelector(".nav-back");
+
+goBack.addEventListener("click", () => {
+   productsMenu.classList.remove("products-open");
+});
+
+// Nav => solid background
+const nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+   let windowPosition = window.scrollY > 0;
+   nav.classList.toggle("scroll-nav", windowPosition);
+});
