5dcae73332525330081af02bf69b4c18d99a49e7
Tommy Hoang <tommyyhoangg@gmail.com>
2020-12-28 11:21:32 +0100

    Added 2 sections to inidivual product page - mobile

---
+++ Templates/Animations/product.js
@@ -0,0 +1,38 @@
+// Nav => solid background
+const nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+   let windowPosition = window.scrollY > 0;
+   nav.classList.toggle("scroll-nav", windowPosition);
+});
+
+// Nav - burger
+const burger = document.querySelector(".burger-menu");
+const burgerMenu = document.querySelector(".nav-menu");
+
+burger.addEventListener("click", () => {
+   burger.classList.toggle("burger-open");
+   burgerMenu.classList.toggle("openM");
+
+   if (burger.classList !== "burger-open") {
+      productsMenu.classList.remove("products-open");
+   }
+
+   if (window.scrollY < 1) {
+      nav.classList.toggle("scroll-nav");
+   }
+});
+
+// Nav - products
+const products = document.querySelector(".link-products");
+const productsMenu = document.querySelector(".nav-products")
+
+products.addEventListener("click", () => {
+   productsMenu.classList.toggle("products-open");
+});
+
+const goBack = document.querySelector(".nav-back");
+
+goBack.addEventListener("click", () => {
+   productsMenu.classList.remove("products-open");
+});
